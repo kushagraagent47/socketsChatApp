a08cf8302eaec63335f52b150da959b6f9cade6a
Kushagra Kumar <kushagraagent47@gmail.com>
2019-12-23 01:52:01 +0530

    Complete Application

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var socket = require('socket.io');
+//App 
+var app = express();
+var server = app.listen(3000);
+console.log("Listening to post 3000");
+
+//Static files
+app.use(express.static('public'));
+
+//Setting up socket
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('Made socket connection');
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data)
+    });
+
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data)
+    });
+});
