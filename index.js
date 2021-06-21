4c65b9554d7dee29f52e67eb4b8128e90bab9df6
Kushagra Kunar <kushagraagent47@gmail.com>
2021-06-21 16:48:45 +0530

    Added backend

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var socket = require('socket.io');
 //App 
 var app = express();
-var server = app.listen(3000);
-console.log("Listening to post 3000");
+var port = process.env.PORT || 8000;
+
+var server = app.listen(port, "0.0.0.0", function () {
+    console.log("Listening on Port 8000");
+});
 
 //Static files
 app.use(express.static('public'));
@@ -11,13 +14,13 @@ app.use(express.static('public'));
 //Setting up socket
 var io = socket(server);
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('Made socket connection');
-    socket.on('chat',function(data){
-        io.sockets.emit('chat',data)
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data)
     });
 
-    socket.on('typing',function(data){
-        socket.broadcast.emit('typing',data)
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data)
     });
 });
